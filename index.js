63b25e0f5a5f3e82474be2809d96326f3adf8f02
David Lambert <david.lambert@my.utsa.edu>
2022-08-20 12:28:53 -0500

    set up movie search bar w/ request

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    //sets up end points to attach at end of url, benefit of axios
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "ffc91e36",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+let timeoutId
+const input = document.querySelector('input')
+//first time through timeoutId is und so setTimeout runs, next time through set timeout has
+// a value so the timer is reset
+const onInput = event => {
+    if (timeoutId){
+        clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 500)
+}
+input.addEventListener('input', onInput)
