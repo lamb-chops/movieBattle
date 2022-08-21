2879cbfe4b7475a9890a727e01213a479f759e03
David Lambert <david.lambert@my.utsa.edu>
2022-08-21 09:40:10 -0500

    finished autocomplete widget

--- autocomplete.js
+++ autocomplete.js
@@ -23,24 +23,24 @@ const createAutoComplete = ({
   // a value so the timer is reset
   //async/await cuz fetchData returns promise so gotta wait for info
   const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
+    const items = await fetchData(event.target.value);
     //closes out drop down is no results
-    if (!movies.length) {
+    if (!items.length) {
       dropdown.classList.remove("is-active");
       return;
     }
     //reset shown results when typing in a different movie
     resultsWrapper.innerHTML = "";
     dropdown.classList.add("is-active"); //css library Bulma command to show dropdown
-    for (let movie of movies) {
+    for (let item of items) {
       const option = document.createElement("a");
       //checks if image has actual source
       option.classList.add("dropdown-item");
-      option.innerHTML = renderOption(movie);
+      option.innerHTML = renderOption(item);
       option.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
-        input.value = inputValue(movie);
-        onOptionSelect(movie);
+        input.value = inputValue(item);
+        onOptionSelect(item);
       });
       resultsWrapper.appendChild(option);
     }
